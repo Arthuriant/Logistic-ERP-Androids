062b3a3861768091b5688a0e6d6724a538dfcffc
faliq1503 <faliqazman2@gmail.com>
2025-04-09 12:08:28 +0700

    User-Report Stock page

--- app/src/main/java/andika/polman/logisticerp2/MainActivity.kt
+++ app/src/main/java/andika/polman/logisticerp2/MainActivity.kt
@@ -5,7 +5,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import andika.polman.logisticerp2.databinding.ActivityMainBinding
 
@@ -30,7 +29,7 @@ class MainActivity : AppCompatActivity() {
                 , R.id.navigation_users
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/andika/polman/logisticerp2/SeconActivity.kt
+++ app/src/main/java/andika/polman/logisticerp2/SeconActivity.kt
@@ -5,7 +5,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import andika.polman.logisticerp2.databinding.ActivitySeconBinding
 
@@ -29,7 +28,7 @@ class SeconActivity : AppCompatActivity() {
                 R.id.navigation_ReportItem, R.id.navigation_IncomingItem, R.id.navigation_ExitItem
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
     }
 }
